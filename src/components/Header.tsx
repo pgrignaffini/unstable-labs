a87455847b868d666ca4b628aa491b28da06c833
grignaffini <grignaffinipaolo@gmail.com>
2022-10-03 16:21:37 +0200

    frontpage

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className='flex justify-between items-center'>
+            <div className="flex items-center cursor-pointer group">
+                <p className="font-pixel font-bold text-3xl text-acid">Unstable<span className="text-white">Labs</span></p>
+                <div className="group-hover:animate-tremble">
+                    <img src="/flask.png" alt="flask" className="w-10" />
+                </div>
+            </div>
+            <p className='font-pixel font-xl text-white cursor-pointer'>Collections</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/ResultCarousel.tsx
@@ -0,0 +1,34 @@
+import Diffemon from "../../data/Diffemon.json"
+import React, { useState } from 'react'
+
+function ResultCarousel() {
+
+    const [imageToShow, setImageToShow] = useState<string>("")
+
+    return (
+        <div>
+            <input type="checkbox" id="diffemon-modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="relative">
+                    <label htmlFor="diffemon-modal" className="absolute right-2 top-2 font-pixel text-2xl text-black cursor-pointer">X</label>
+                    <img className='w-full h-full object-cover' src={imageToShow} alt="banner" />
+                </div>
+            </div>
+            <div className='bg-gray-400 p-4 overflow-x-auto flex space-x-4'>
+                {Diffemon.map((_, index: number) => (
+                    <div key={index} onClick={() => setImageToShow(`/diffemon/${index}.png`)}>
+                        <div className='bg-gray-200 p-4 w-60 h-124 flex-none shadow-xl'>
+                            <label htmlFor="diffemon-modal" className='cursor-pointer'>
+                                <img src={`/diffemon/${index}.png`}
+                                />
+                            </label>
+                            <button className="bg-acid text-white p-4 font-pixel cursor-pointer mt-4 w-full">Mint</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ResultCarousel
